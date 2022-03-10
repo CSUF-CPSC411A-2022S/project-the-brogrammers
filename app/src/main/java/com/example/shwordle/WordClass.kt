1311dee842f721888aa75959d98a8f10af0d6ca4
Tdttran2000 <55966502+Tdttran2000@users.noreply.github.com>
2022-03-10 00:55:22 -0800

    Added Word class, as well as a child class that will inherit from it.

---
+++ app/src/main/java/com/example/shwordle/WordClass.kt
@@ -0,0 +1,12 @@
+package com.example.shwordle
+
+open class Word(val spelling: String, length: Int) {
+    /*
+    * Need to implement check for making
+    * */
+}
+
+class KeyWord(spelling: String, length: Int,
+              correctletter: Array<Char>):
+    Word(spelling, length) {
+}
